e260b7a8a6122aa6358a2426ef9fdb4fccfae7df
Macguire Rintoul <mrintoul@users.noreply.github.com>
2018-07-16 22:08:11 -0700

    retrieving numbers

--- app/src/main/java/com/mrintoul/macguirerintoul_a3/MainActivity.java
+++ app/src/main/java/com/mrintoul/macguirerintoul_a3/MainActivity.java
@@ -1,13 +1,110 @@
 package com.mrintoul.macguirerintoul_a3;
 
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button retrieveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrieveButton = findViewById(R.id.retrieve);
+        retrieveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.retrieve:
+                new RetrieveNumbersTask().execute("http://qrng.anu.edu.au/API/jsonI.php?length=4&type=uint8");
+                break;
+        }
+    }
+
+    private String readJSONData(String myurl) throws IOException {
+        InputStream is = null;
+        int len = 2500;
+
+        URL url = new URL(myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        try {
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("tag", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+                conn.disconnect();
+            }
+        }
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    class RetrieveNumbersTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+            try {
+                return readJSONData(urls[0]);
+            } catch(IOException e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray numberData = jsonObject.getJSONArray("data");
+                Toast.makeText(getBaseContext(), String.valueOf(numberData.get(1)), Toast.LENGTH_SHORT).show();
+
+                // set each textview as the numbers
+            } catch (Exception e) {
+                Log.d("exception", "onPostExecute");
+                e.printStackTrace();
+            }
+        }
     }
 }
